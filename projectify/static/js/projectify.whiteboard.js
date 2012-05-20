2aa70c5a21df64e3ca575e7246505942a73c9bc9
Georgi Kostadinov <grgkostadinov@gmail.com>
2012-05-20 03:06:46 +0300

    Live update - whiteboard.

--- projectify/static/js/projectify.whiteboard.js
+++ projectify/static/js/projectify.whiteboard.js
@@ -10,29 +10,44 @@ APE.whiteboardChangeListen = DUI.Class.create(APE.Client.prototype, {
 
 		this.addEvent('load', this.start);
 		this.addEvent('ready', this.join);
-		this.addEvenet('multiPipeCreate', this.createPipe);
+		this.addEvent('multiPipeCreate', this.createPipe);
 
-		this.onRaw('data', this.moveObjects);
+		this.onRaw('data', this.onRawEvent);
 	},
 
 	start: function()
 	{
+		console.log(1)
 		this.core.start({'name':'User'+String(new Date().getTime()).substr(5)})
 	},
 
 	join: function()
     {
+		console.log(2)
         this.core.join(this.options.channel);
     },
 
-    moveObjects: function(raw, pipe)
+    onRawEvent: function(raw, pipe)
     {
+		console.log(4)
         msg = $.parseJSON(decodeURIComponent(raw.data.msg));
-        console.log(msg);
+
+        if (msg.create)
+        {
+        	addSticky(msg);
+        }
+        else
+        {
+        	//console.log(msg)
+        	var whiteboard_offset = $("#whiteboard").offset();
+        	$('#' + msg.elementId).css({'top': whiteboard_offset.top + msg.position.top, 'left': whiteboard_offset.left + msg.position.left})
+        }
     },
 
     createPipe: function(pipe, options)
     {
+
+		console.log(3)
         this.currentPipe = pipe;
     },
 
